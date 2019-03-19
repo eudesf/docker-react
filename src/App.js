7d3e0af01e76f6f28ac275737b5ea9d249af20af
Eudes Lima <aeudes@gmail.com>
2019-03-19 18:19:46 -0300

    change app text

--- src/App.js
+++ src/App.js
@@ -9,16 +9,8 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Hi there again!
+            I was changed from the feature branch!
           </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
         </header>
       </div>
     );
